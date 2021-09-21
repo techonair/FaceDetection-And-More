3e982a00b3cf78e0cedc7c5ceef8c2f618766bd1
Bhanu Pratap <techieonmail@gmail.com>
2021-09-21 23:12:33 +0530

    Optimized code

--- Face Detection.py
+++ Face Detection.py
@@ -8,6 +8,4 @@ while True:
     _, img = cam.read()
     img = cv2.resize(img, dsize= 0, fx = 0.5, fy = 0.5)
     grayImg = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2GRAY)
-    gaussianBlur = cv2.GaussianBlur(grayImg, (21,21), 0, 2)
-    x , y, w, h = cv2.CascadeClassifier(haarfile)
-    cv2.boundingRect(img, (x, y, w, h), color = (0,255, 0), 1)
+    gaussianBlur = cv2.GaussianBlur(grayImg, (21,21), 0, 2

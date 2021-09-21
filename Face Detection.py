96193ba8dfe3cadd17e32768eee2d53f88d27fbd
Bhanu Pratap <techieonmail@gmail.com>
2021-09-21 23:14:53 +0530

    deleted some garbage code

--- Face Detection.py
+++ Face Detection.py
@@ -3,9 +3,3 @@ import cv2, os
 haarfile = "E:\FaceDetection-And-More\Haarcascade\haarcascade_eye.xml"
 
 cam = cv2.VideoCapture(0)
-
-while True:
-    _, img = cam.read()
-    img = cv2.resize(img, dsize= 0, fx = 0.5, fy = 0.5)
-    grayImg = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2GRAY)
-    gaussianBlur = cv2.GaussianBlur(grayImg, (21,21), 0, 2

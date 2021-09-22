87e616f3f6940da9f9e48408b1faf260c63a9025
Bhanu Pratap <techieonmail@gmail.com>
2021-09-22 23:14:25 +0530

    code for model initialization

--- Face Data Collection.py
+++ Face Data Collection.py
@@ -3,18 +3,28 @@ import cv2, os
 dataset = 'dataset'
 me = 'me'
 
-#Looking for path and directories, if doesn't exists it creates
+# Looking for path and directories, if doesn't exists it creates
 path = os.path.join(dataset, me)
 if not os.path.isdir(path):
     os.makedirs(path)
 
+# Loading algo
 FaceAlgo = "E:\\FaceDetection-And-More\\Haarcascade\\haarcascade_frontalface_default.xml"
+# This is a smart solution (applying limited feature out of 6000 in stages)
+# used to find only the possible pixels containing face
+# and then applying remaining 6000 feature in stages to the area selected
 haarFaceClassifier = cv2.CascadeClassifier(FaceAlgo)
 
 cam = cv2.VideoCapture(0)
 
-count = 0
+count = 1
 
+while count<31:
+    _,img = cam.read()
+    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 'detectMultiScale' is used for detecting 
+    FaceOnly = haarFaceClassifier.detectMultiScale(grayImg, 1.3, 5)
+    
 
 
 

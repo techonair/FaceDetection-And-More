384d075fbab8d4501bf25be3a76ec0bd266a659a
Bhanu Pratap <techieonmail@gmail.com>
2021-09-22 20:45:56 +0530

    Rewrote the code to make directories and joined path for collecting face data in them

--- Face Detection.py
+++ Face Detection.py
@@ -1,5 +1,20 @@
 import cv2, os
 
-haarfile = "E:\FaceDetection-And-More\Haarcascade\haarcascade_eye.xml"
+dataset = 'dataset'
+me = 'me'
+
+#Looking for path and directories, if doesn't exists it creates
+path = os.path.join(dataset, me)
+if not os.path.isdir(path):
+    os.makedirs(path)
+
+FaceAlgo = "E:\\FaceDetection-And-More\\Haarcascade\\haarcascade_frontalface_default.xml"
+haarFaceClassifier = cv2.CascadeClassifier(FaceAlgo)
 
 cam = cv2.VideoCapture(0)
+
+count = 0
+
+
+
+

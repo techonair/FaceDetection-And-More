41dfe6b798714765a1816545cc54f84ed2343a30
Bhanu Pratap <techieonmail@gmail.com>
2021-09-23 23:58:05 +0530

    added 'text' feature when face & eyes detected

--- Face+Eye_Detection.py
+++ Face+Eye_Detection.py
@@ -10,12 +10,19 @@ cam = cv2.VideoCapture(0)
 
 while True:
     _, img = cam.read()
+    resize_img = cv2.resize(img)
     text = "No Face Detected"
-    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grayImg = cv2.cvtColor(resize_img, cv2.COLOR_BGR2GRAY)
     face_feature = FaceDetect.detectMultiScale(grayImg, 1.3, 5)
     eye_feature = EyeDetect.detectMultiScale(grayImg, 1.3, 5)
     for (x,y,w,h) in face_feature:
+        text = "Face Detected"
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        
     for (a,b) in eye_feature:
+        eyetext = "Eyes Detected"
         cv2.circle(img, (a,b), 2, (0,0,255), 2)
         
+    print(text)
+    cv2.putText(img, text, (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color=(0,255,0), 2)
+    cv2.putText(img, eyetext, (480,490) ,cv2.FONT_HERSHEY_SIMPLEX, 0.5, color=(0,255,0), 2)
